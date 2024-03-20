0c62f8f08f97c0d5d60a7b18d2df7161b97db21d
ddiniz-m <diogodinizm@gmail.com>
2024-03-20 19:09:27 +0000

    Started cpp08

---
+++ cpp08/ex00/inc/easyfind.hpp
@@ -0,0 +1,13 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <vector>
+#include <iostream>
+
+template <typename T> T easyfind(T a, int b)
+{
+	(void)b;
+	return (a);
+}
+
+#endif
---
+++ cpp08/ex00/src/main.cpp
@@ -0,0 +1,8 @@
+#include "../inc/easyfind.hpp"
+
+int	main(void)
+{
+	std::vector<int> v(1, 10);
+
+	std::cout << easyfind(v[0], 3) << "\n";
+}
